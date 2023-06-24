e13501e836f042caf84470492cdd49e78cc7fca5
Daksh <dakshnahar2001@gmail.com>
2023-06-25 01:09:04 +0530

    server.js change

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 
 require('dotenv').config()
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({extended:true}));
 var jsonParser = bodyParser.json()
